31ba96e0ebdda28196c24640348ec7cd8995fbfc
Mathijs <Mathijs>
2023-12-02 16:07:38 +0100

    fix names

---
+++ backend/src/DAO/eventsDao.ts
@@ -0,0 +1,57 @@
+import { MongoClient, Collection } from "mongodb";
+import { ObjectId, UUID } from "bson";
+import log from "../logger";
+import LogLevel from "../enums/logLevel";
+import SchoolEvent from "../interfaces/event";
+
+
+
+let events: Collection<SchoolEvent>;
+
+/**
+ * Data Access Object for events
+ * 
+ */
+export default class EventsDao {
+    static async injectDB(conn: MongoClient) {
+        if (events) {
+            return;
+        }
+        try {
+            events = await conn.db(process.env.DB_NAME).collection("events");
+        } catch (e) {
+            log(LogLevel.Error, `Unable to establish collection handles in eventsDAO: ${e}`);
+        }
+    }
+
+
+    /**
+     * Returns all events in the database
+     * @returns {SchoolEvent[] | null} Returns an array of SchoolEvents or null if an error occured
+     */
+    static async getSchoolEvents() {
+        try {
+            return await events.find().toArray();
+        } catch (e) {
+            log(LogLevel.Error, `Unable to issue find command in events, ${e}`);
+            return null;
+        }
+    }
+
+
+    /**
+     * Inserts a new event into the database
+     * @param event The event to insert into the database
+     * @returns {InsertOneResult | null} Returns the result of the insert command or null if an error occured
+     */
+
+    static async insertSchoolEvent(event: SchoolEvent) 
+    { 
+        try {
+            return await events.insertOne(event);
+        } catch (e) {
+            log(LogLevel.Error, `Unable to issue insert command in events, ${e}`);
+            return null;
+        }
+    }
+}
---
+++ backend/src/DAO/loginConfirmationsDao.ts
@@ -0,0 +1,55 @@
+import { MongoClient, Collection } from "mongodb";
+import { ObjectId, UUID } from "bson";
+import log from "../logger";
+import LogLevel from "../enums/logLevel";
+import confirmationToken from "../interfaces/confirmationToken";
+
+
+let confirmationTokens: Collection<confirmationToken>;
+
+/**
+ * Data Access Object for users. This code interacts directly with the database.
+ */
+export default class loginConfirmationsDAO {
+    /**
+     * This function is called when the server starts.
+     * It passes the connection over to this class so it can be used to interact with the database.
+     * @param conn MongoDB connection
+     * @returns 
+     */
+    static async injectDB(conn: MongoClient) {
+        if (confirmationTokens) {
+            return;
+        }
+        try {
+            confirmationTokens = await conn.db(process.env.DB_NAME).collection("loginConfirmationTokens");
+        } catch (e) {
+            log(LogLevel.Error, `Unable to establish collection handles in loginConfirmationDAO: ${e}`);
+        }
+    }
+
+    /**
+     * Gets a confirmation token by its ID.
+     * @param id confirmation token ID
+     * @returns confirmation token
+     */
+    static async getConfirmationTokenById(id: UUID) {
+        try {
+            return await confirmationTokens.findOne({_id: id});
+        } catch (e) {
+            log(LogLevel.Debug, `Unable to get confirmation token: ${e}`);
+            return null;
+        }
+    }
+
+
+    static async insertConfirmationToken(token: confirmationToken) {
+        try {
+            return await confirmationTokens.insertOne(token);
+        } catch (e) {
+            log(LogLevel.Debug, `Unable to insert confirmation token: ${e}`);
+            return null;
+        }
+    }
+    
+}
