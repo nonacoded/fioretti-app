e05fcec6ccbb04e0fddeda85a349e791222d01d3
Ikbenmathijs <51931123+Ikbenmathijs@users.noreply.github.com>
2024-01-10 12:57:59 +0100

    create qr scanning page + add camera permissions

---
+++ fioretti_app/lib/pages/qr_scanner.dart
@@ -0,0 +1,16 @@
+import 'package:fioretti_app/widgets/scaffold.dart';
+import 'package:flutter/material.dart';
+
+class QrScanningPage extends StatefulWidget {
+  const QrScanningPage({super.key});
+
+  @override
+  State<QrScanningPage> createState() => _QrScanningPageState();
+}
+
+class _QrScanningPageState extends State<QrScanningPage> {
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(body: Placeholder());
+  }
+}
