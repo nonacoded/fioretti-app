ff735c3eee7d12cf7ffaff82efefae9c7449fe3f
lara <a>
2024-02-15 12:03:23 +0100

    profielpage

---
+++ fioretti_app/lib/pages/profiel_page.dart
@@ -0,0 +1,41 @@
+import 'package:fioretti_app/widgets/scaffold.dart';
+import 'package:flutter/material.dart';
+import 'package:fioretti_app/models/ticket.dart';
+import 'package:fioretti_app/widgets/ticket_card.dart';
+
+class ProfielPage extends StatefulWidget {
+  const ProfielPage({super.key});
+
+  @override
+  State<ProfielPage> createState() => _ProfielPageState();
+}
+
+class _ProfielPageState extends State<ProfielPage> {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      title: "Profiel",
+      body: Column(
+        children: [
+                // title
+                Text("Profiel",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                    )),
+
+        Row(
+                  children: [
+                    const Icon(Icons.person),
+                    Text("gegevens",
+                        style: const TextStyle(
+                            fontSize: 13.0, fontStyle: FontStyle.italic)),
+                  ],
+                ),
+        ],
+      ),
+    ),
+  }
+}
