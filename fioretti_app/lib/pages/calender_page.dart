265785824e1f219524e79792b9982204d0aa33d3
lara <a>
2024-02-29 11:18:16 +0100

    kalender page

---
+++ fioretti_app/lib/pages/calender_page.dart
@@ -0,0 +1,52 @@
+import 'package:fioretti_app/widgets/scaffold.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fioretti_app/providers.dart';
+import "package:go_router/go_router.dart";
+import "package:fioretti_app/models/user.dart";
+
+class CalenderPage extends ConsumerStatefulWidget {
+  const CalenderPage({super.key});
+
+  @override
+  ConsumerState<CalenderPage> createState() => _CalenderPageState();
+}
+
+class _CalenderPageState extends ConsumerState<CalenderPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    User? user = ref.read(userProvider);
+
+    if (user == null) {
+      context.go("/");
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User? user = ref.watch(userProvider);
+
+    if (user == null) {
+      return const Text(
+          "Geen gebruiker gevonden, dit is een bug. Probeer de app opnieuw te starten.");
+    }
+
+    return AppScaffold(
+      title: "Profiel",
+      body: Column(
+        children: [
+          // title
+          const Text("Kalender",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              )),
+          Text("Welkom ${user.firstName}!"),
+        ],
+      ),
+    );
+  }
+}
