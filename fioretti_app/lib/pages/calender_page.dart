d1e5580612313c4e60915ae673348999727479ba
lara <a>
2024-02-29 15:45:13 +0100

    tijd calender

--- fioretti_app/lib/pages/calender_page.dart
+++ fioretti_app/lib/pages/calender_page.dart
@@ -38,7 +38,67 @@ class _CalenderPageState extends ConsumerState<CalenderPage> {
       title: "Kalender",
         body: ListView(
         children: List.generate(10, (index) {
-          return Placeholder();
+          /*return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => EvenementDetailsPage(
+                details: EvenementDetails(
+                  title: title,
+                  description: description,
+                ),
+              ),
+            ),
+          );
+        },
+        child: Card(
+          margin: EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Maand en Dag labels
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      month,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(day),
+                  ],
+                ),
+                SizedBox(
+                    width:
+                        10), // Spatie tussen Maand/Dag en de titel/beschrijving
+                // Titel en beschrijving
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      SizedBox(
+                          height: 5), // Spatie tussen titel en beschrijving
+                      Text(description),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));*/
+        String dateTimeToString(DateTime dateTime) {
+  return "${dateTime.day}/${dateTime.month}/${dateTime.year}";
+}
+         // return Placeholder();
           // Hier maak je een EventItemWidget voor elk evenement
           /*return EventItemWidget(
             month: 'Maand',
