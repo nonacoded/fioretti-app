8e27616db289e16f2ae5defb7fde9d294572583c
lara <a>
2024-03-15 10:33:42 +0100

    a

--- fioretti_app/lib/pages/event_page.dart
+++ fioretti_app/lib/pages/event_page.dart
@@ -54,7 +54,6 @@ class _EventPageState extends State<EventPage> {
                 ? "Bezig met laden..."
                 : "Evenement niet gevonden")
             : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   EventDisplay(event: event!),
                   ElevatedButton(
