3487c3dedb9fe3ef383fdefbe81db4dcea42f55b
lara <a>
2024-03-15 12:04:57 +0100

    d

--- fioretti_app/lib/functions/utils.dart
+++ fioretti_app/lib/functions/utils.dart
@@ -1,44 +1,52 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:fioretti_app/main.dart';
+import 'package:intl/intl.dart'; // Importeer de intl library
 
-String minuutNaarString(int minuut) {
+String minuteToString(int minuut) {
   return minuut < 10 ? "0$minuut" : "$minuut";
 }
 
-String datumTijdNaarString(DateTime datumTijd) {
-  final format = DateFormat('EEEE, d MMMM y', 'nl_NL'); // Voor Nederlandse datums
-  return "${format.format(datumTijd)} ${datumTijd.hour}:${minuutNaarString(datumTijd.minute)}";
+String dateTimeToString(DateTime dateTime) {
+  // Gebruik DateFormat van intl om de datum inclusief weekdag te formatteren
+  final formatter = DateFormat('EEEE, d MMMM y', 'nl_NL'); // Voor Nederlandse format, pas 'nl_NL' aan naar je locale indien nodig
+  return formatter.format(dateTime) + " ${dateTime.hour}:${minuteToString(dateTime.minute)}";
 }
 
-String datumNaarString(DateTime datum) {
-  final format = DateFormat('EEEE, d MMMM y', 'nl_NL'); // Voor Nederlandse datums
-  return format.format(datum);
+String dateToString(DateTime datum) {
+  final formatter = DateFormat('EEEE, d MMMM y', 'nl_NL'); // Voor Nederlandse format
+  return formatter.format(datum);
 }
 
-String tijdNaarString(DateTime tijd) {
-  return "${tijd.hour}:${minuutNaarString(tijd.minute)}";
+String timeToString(DateTime tijd) {
+  return "${tijd.hour}:${minuteToString(tijd.minute)}";
 }
 
-String foutMeldingUitBody(dynamic body) {
+String getErrorMessageFromBody(dynamic body) {
   try {
-    final json = jsonDecode(body) as Map<String, dynamic>;
-    if (json.containsKey('message') && json['message'] is String) {
-      return json['message'];
+    Map<String, dynamic> json = jsonDecode(body);
+    if (json.containsKey("message") && json["message"] is String) {
+      return json["message"];
     }
-  } catch (_) {}
-  return body is String ? body : '';
+  } catch (e) {
+    // Voeg een catch block toe voor het geval jsonDecode faalt
+    print("Error parsing JSON: $e");
+  }
+
+  if (body is String) {
+    return body;
+  }
+  return "";
 }
 
-void toonSnackBar(String bericht) {
-  final snackBar = SnackBar(
-    content: Text(bericht),
-    duration: const Duration(seconds: 10),
-  );
-
-  scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
-}
+// void showSnackBar(String bericht) {
+//   final currentState = scaffoldMessengerKey.currentState;
+//   if (currentState != null) {
+//     currentState.showSnackBar(SnackBar(
+//       content: Text(bericht),
+//       duration: const Duration(seconds: 10),
+//     ));
+//   }
+// }
 
 
 
