2f79ffd224b2e6af14f2fe9d51a82de6309f8368
lara <a>
2024-03-15 10:27:54 +0100

    w

--- fioretti_app/lib/widgets/event_display.dart
+++ fioretti_app/lib/widgets/event_display.dart
@@ -15,7 +15,7 @@ class EventDisplay extends StatelessWidget {
     return Stack(
       children: [
         Container(
-            width: width * 0.8,
+            width: width * 0.4,
             height: height * 0.4,
             child: Column(
               children: [
