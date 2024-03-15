c5725925416eb4313c7c38025b75acf33c1d1388
lara <a>
2024-03-15 21:22:53 +0100

    ka

--- fioretti_app/lib/widgets/scaffold.dart
+++ fioretti_app/lib/widgets/scaffold.dart
@@ -47,7 +47,7 @@ class AppScaffoldState extends ConsumerState<AppScaffold> {
 
     Scaffold result = Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue[900],
+        backgroundColor: const Color.fromRGBO(37, 53, 125, 100),
           title: Text(
             widget.title,
             style: TextStyle(color: Colors.white),
@@ -99,7 +99,7 @@ class AppScaffoldState extends ConsumerState<AppScaffold> {
             context.go("/calendar"); 
           }         
         },
-        backgroundColor: const Color.fromRGBO(37, 53, 125, 100),
+        backgroundColor: Colors.lightBlue[900],
       ),
     );
 /*let op index navigationbar*/
