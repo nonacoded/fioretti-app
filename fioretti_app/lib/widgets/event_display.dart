f82df045e39166594271a4b4b173c3d435a3440f
lara <a>
2024-03-17 18:14:57 +0100

    ?

--- fioretti_app/lib/widgets/event_display.dart
+++ fioretti_app/lib/widgets/event_display.dart
@@ -58,9 +58,9 @@ class EventDisplay extends StatelessWidget {
                      Row(
                       children:[
                        const  Icon(Icons.schedule),
-                         Text("${timeToString(event.time)}",
-                          style:  const TextStyle(
-                            fontSize: 13.0, fontStyle: FontStyle.italic))]),
+                        //  Text("${timeToString(event.time)}",
+                        //   style:  const TextStyle(
+                        //     fontSize: 13.0, fontStyle: FontStyle.italic))]),
                     const SizedBox(height: 10),
                     Row( 
                       children:[ 
