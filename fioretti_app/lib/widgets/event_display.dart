a8620201aa6499b4ae5b43bd157670b338b475ed
lara <a>
2024-03-17 13:10:57 +0100

    t

--- fioretti_app/lib/widgets/event_display.dart
+++ fioretti_app/lib/widgets/event_display.dart
@@ -1,15 +1,11 @@
 import 'package:fioretti_app/functions/utils.dart';
 import 'package:fioretti_app/models/school_event.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:fioretti_app/models/ticket.dart';
-import 'package:fioretti_app/widgets/scaffold.dart';
 
 class EventDisplay extends StatelessWidget {
   final SchoolEvent event;
-  final Ticket ticket;
 
-  const EventDisplay({super.key, required this.event, required this.ticket});
+  const EventDisplay({super.key, required this.event});
 
   @override
   Widget build(BuildContext context) {
@@ -77,14 +73,7 @@ class EventDisplay extends StatelessWidget {
                         Text(event.location,
                           style: const TextStyle(
                             fontSize: 13.0, fontStyle: FontStyle.italic))]),
-                            const SizedBox(height: 10),
-                  Row(
-                    children:[  
-                      QrImageView(
-          data: ticket.id,
-          version: QrVersions.auto,
-          size: 100.0,
-        ),],),],
+                  ],
                 ),
               ],
             ))
