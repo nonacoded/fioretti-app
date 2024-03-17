b82be5fa5a3652b52180a0299999622b62e188ce
lara <a>
2024-03-17 11:52:46 +0100

    sb

--- fioretti_app/lib/widgets/event_display.dart
+++ fioretti_app/lib/widgets/event_display.dart
@@ -45,7 +45,7 @@ class EventDisplay extends StatelessWidget {
       )
     ],
   ), ), ),  
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 20),
                    
                     Row(
                       children:[
