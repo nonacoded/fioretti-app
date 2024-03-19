bd3d1d63982a8e8a75fb1a6119c5995542468000
lara <a>
2024-03-19 20:24:59 +0100

    all

--- fioretti_app/lib/pages/profiel_page.dart
+++ fioretti_app/lib/pages/profiel_page.dart
@@ -67,8 +67,7 @@ double height = MediaQuery.of(context).size.height;
           ),
           const SizedBox(height: 10),
           Container(
-            color: Colors.blue,
-            margin: const EdgeInsets.only(left: 30.0),
+            margin: const EdgeInsets.all(30.0),
             child: const LogoutButton(),)
         ]
       ),
