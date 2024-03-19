af088f563bd600b3fb4b0b178ae6cbe594d7c5a8
lara <a>
2024-03-19 10:55:25 +0100

    terug

--- fioretti_app/lib/widgets/event_display.dart
+++ fioretti_app/lib/widgets/event_display.dart
@@ -1,6 +1,5 @@
 import 'package:fioretti_app/functions/utils.dart';
 import 'package:fioretti_app/models/school_event.dart';
-import 'package:fioretti_app/widgets/scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -14,24 +13,9 @@ class EventDisplay extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
-    //return Stack(
-      return Scaffold(
-        appBar: AppBar(
-        title: const Text("Evenement info", style: TextStyle(color: Colors.white),),
-        centerTitle: true,
-          leading: GestureDetector(
-     child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Image.asset(
-                'assets/logo.png',
-                width: 60, // Adjust the width to your desired size
-                height: 60, // Adjust the height to your desired size// Added semantic label for accessibility
-              ),
-            ),
-          ),
-        backgroundColor: Colors.lightBlue[900],
-      ),
-      body: Container(
+    return Stack(
+      children: [
+        Container(
             width: width * 0.9,
             height: height * 0.6,
             child: Column(
@@ -93,7 +77,8 @@ class EventDisplay extends StatelessWidget {
                   ],
                 ),
               ],
-            )),
+            ))
+      ],
     );
   }
 }
