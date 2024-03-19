60e264b9372e64596e74cf261ed97a28668a4fd5
lara <a>
2024-03-19 20:13:41 +0100

    c

--- fioretti_app/lib/pages/profiel_page.dart
+++ fioretti_app/lib/pages/profiel_page.dart
@@ -46,7 +46,7 @@ double height = MediaQuery.of(context).size.height;
               children: [
                 // title
                 const SizedBox(height: 20),
-                Align(alignment: Alignment.centerRight,
+                Align(alignment: Alignment.center,
                 child: Container(child: Text("Mijn gegevens",
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
