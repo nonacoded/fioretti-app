55866893ecddce31a201c69fa4cac7f9c1ada9fd
nonacoded <bartsalm1@gmail.com>
2024-11-18 15:40:24 +0100

    login can now build

--- login_website_fioretti_app/src/app/loggedin/page.tsx
+++ login_website_fioretti_app/src/app/loggedin/page.tsx
@@ -1,39 +1,36 @@
 'use client';
 import { useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from 'react';
 
-export default function LoggedInPage() {
+function LoggedInContent() {
     const searchParams = useSearchParams();
     const router = useRouter();
 
-
     useEffect(() => {
-        if (!searchParams.has("token")) {
+        if (!searchParams.has("token") || !searchParams.has("expires")) {
             router.push("/");
-        }
-        if (!searchParams.has("expires")) {
+        } else if (parseInt(searchParams.get("expires") as string) < Date.now()) {
             router.push("/");
+        } else {
+            router.push(`fiorettiapp://fiorettiapplink.nl/login?token=${searchParams.get("token")}&expires=${searchParams.get("expires")}`);
         }
-        if (parseInt(searchParams.get("expires") as string) < Date.now()) {
-            router.push("/");
-        }
-
-        router.push(`fiorettiapp://fiorettiapplink.nl/login?token=${searchParams.get("token")}&expires=${searchParams.get("expires")}`);
-    })
+    }, [searchParams, router]);
 
     return (
-        <>
-          <div className="grid static place-content-center h-screen w-screen">
-              <div className="static bg-white rounded-lg p-7">
+        <div className="grid static place-content-center h-screen w-screen">
+            <div className="static bg-white rounded-lg p-7">
                 <h1 className="text-4xl text-slate-600 text-center font-medium pb-4">✅Je bent ingelogd!</h1>
                 <h2 className="text-lg text-slate-500 text-center pb-7">Open deze pagina in de fioretti app om toegang te krijgen</h2>
-                <div className="grid static place-content-center">
-                
-                </div>
-              </div>
-          </div>
-    
-         
-        </>
-      )
+            </div>
+        </div>
+    );
+}
+
+export default function LoggedInPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <LoggedInContent />
+        </Suspense>
+    );
 }
